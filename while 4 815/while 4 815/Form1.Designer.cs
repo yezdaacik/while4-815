7e1fa81ea47b4f75e1ba37df90fcb342d45d46f6
Yezda <145451081+yezdanuracik@users.noreply.github.com>
2023-11-25 16:26:26 +0300

    Add files via upload

---
+++ while 4 815/while 4 815/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace while_4_815
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtxtSayi = new System.Windows.Forms.RichTextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtxtSayi
+            // 
+            this.rtxtSayi.Location = new System.Drawing.Point(35, 12);
+            this.rtxtSayi.Name = "rtxtSayi";
+            this.rtxtSayi.Size = new System.Drawing.Size(189, 81);
+            this.rtxtSayi.TabIndex = 0;
+            this.rtxtSayi.Text = "";
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(79, 131);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(95, 35);
+            this.btnEkle.TabIndex = 1;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(285, 205);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.rtxtSayi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtxtSayi;
+        private System.Windows.Forms.Button btnEkle;
+    }
+}
+
---
+++ while 4 815/while 4 815/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace while_4_815
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            int sayi = 0;
+            do
+            {
+                rtxtSayi.Text = rtxtSayi.Text + sayi.ToString();
+                sayi++;
+            } while (sayi <= 30);
+        }
+    }
+}
